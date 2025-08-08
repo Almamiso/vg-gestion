3d2fbc3df9fc46efce1bb0ae2b0a3ea279caa21a
almamiso <neron.alex@gmail.com>
2025-08-08 11:41:30 +0200

    chore(db): initial migrations

---
+++ alembic/versions/ce0bdee5a4cd_init.py
@@ -0,0 +1,20 @@
+"""
+Revision ID: ce0bdee5a4cd
+Revises: None
+Create Date: 2025-08-08 09:40:32.921490
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'ce0bdee5a4cd'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
